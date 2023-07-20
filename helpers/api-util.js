d9e6b76e63bee4afa60bee81690becdcb7324cf3
Stanislav Prykhodko <stasprykhodko@gmail.com>
2023-07-21 02:41:56 +0300

    test prev link

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,6 +1,6 @@
 export async function getAllEvents() {
-  // const response = await fetch('https://nextjs-newproj-default-rtdb.firebaseio.com/events.json')
-  const response = await fetch('https://nextjs-newproj-66e0d-default-rtdb.firebaseio.com/events.json')
+  const response = await fetch('https://nextjs-newproj-default-rtdb.firebaseio.com/events.json')
+  // const response = await fetch('https://nextjs-newproj-66e0d-default-rtdb.firebaseio.com/events.json')
   const data = await response.json()
 
   const eventsToArr = [];
